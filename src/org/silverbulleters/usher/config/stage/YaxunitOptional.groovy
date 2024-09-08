5f5182e1cfc7b8aca5985b0e20115a1434f68b86
Oleg Karataev <test@mail.ru>
2024-09-08 14:03:52 +0300

    YaxunitOptional

---
+++ src/org/silverbulleters/usher/config/stage/YaxunitOptional.groovy
@@ -0,0 +1,29 @@
+/*
+ * Vanessa-Usher
+ * Copyright (C) 2019-2022 SilverBulleters, LLC - All Rights Reserved.
+ * Unauthorized copying of this file in any way is strictly prohibited.
+ * Proprietary and confidential.
+ */
+package org.silverbulleters.usher.config.stage
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonPropertyDescription
+
+/**
+ * Настройки этапа yaxunit
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+class YaxunitOptional extends BaseOptional {
+  @JsonPropertyDescription("Путь к каталогу выгрузки отчета в формате Allure. Например, `./out/yaxunitallure`")
+  String allurePath = "./out/yaxunitallure"
+
+  @JsonPropertyDescription("Путь к файлу настроек фреймворка тестирования ./tools/JSON/yaxunit.json")
+  String yaxunitsettings = './tools/JSON/yaxunit.json'
+
+  YaxunitOptional() {
+    name = 'Yaxunit'
+    id = 'yaxunit'
+    timeout = 100
+  }
+
+}
